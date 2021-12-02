404b49f4b48392c6bd476f3929eaeae5fab1910f
Louis <35641311+Loule95450@users.noreply.github.com>
2021-12-02 10:04:01 +0100

    :mega: - Add comments

--- src/main/java/me/loule/bmi/controller/MainController.java
+++ src/main/java/me/loule/bmi/controller/MainController.java
@@ -28,18 +28,18 @@ public class MainController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         buttonCalculate.setOnMouseClicked(event -> {
-            BmiModel bmi = new BmiModel(Double.parseDouble(inputSize.getText()), Double.parseDouble(inputWeight.getText()));
+            BmiModel bmi = new BmiModel(Double.parseDouble(inputSize.getText()), Double.parseDouble(inputWeight.getText())); // Call Model to get the result
 
-            String result = "Your BMI is " + (int) Math.round(bmi.getBmi()) + ". You are ";
+            String result = "Your BMI is " + (int) Math.round(bmi.getBmi()) + ". You are "; // Set the result text
 
             if (bmi.getBmi() <= 18.5) {
-                textResult.setText(result + "underweight.");
+                textResult.setText(result + "underweight."); // Underweight
             } else if (bmi.getBmi() <= 25) {
-                textResult.setText(result + "normal.");
+                textResult.setText(result + "normal."); // Normal
             } else if (bmi.getBmi() <= 30) {
-                textResult.setText(result + "overweight.");
+                textResult.setText(result + "overweight."); // Overweight
             } else {
-                textResult.setText(result + "obese.");
+                textResult.setText(result + "obese."); // Obese
             }
         });
     }
